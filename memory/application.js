47432dde25ec3f48216bb2d919148ebd23cace38
jeiben <jeiben@kippnyc.org>
2015-03-01 13:42:59 -0500

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$(".card").on("click", function() {
 		event.preventDefault();
-		$(this).find("p").fadeIn().delay(1000).fadeOut();
+		$(this).find("p").show().delay(1000).hide();
 	});
 });
