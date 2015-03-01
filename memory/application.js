345481493432ab6e2265c7b101e9d2e647ec66dd
jeiben <jeiben@kippnyc.org>
2015-03-01 14:16:22 -0500

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,6 +1,16 @@
 $(document).ready(function(){
 	event.preventDefault();
 	$(".card").on("click", function() {
+		var first_guess = $(this).find("p").html();
+		$(this).find("p").addClass("clicked");
 		$(this).find("p").css("opacity", 1);
+		$.when($(".card").on("click", function() {
+			var second_guess = $(this).find("p").html();
+			$(this).find("p").css("opacity", 1);
+			$(this).find("p").addClass("clicked");
+			if (first_guess != second_guess){
+				$(".clicked").each().css("opacity, 0");
+			};
+		}))
 	});
 });
