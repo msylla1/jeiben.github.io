c8f795ea60c911d26c25f5ed68c39bc2dd14ee71
jeiben <jeiben@kippnyc.org>
2015-03-01 14:54:14 -0500

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,25 +1,25 @@
 $(document).ready(function(){
-	event.preventDefault();
-	var click_count = 0;
-	var first_guess = 0;
-	var second_guess = 0;
-	$(".card").on("click", function() {
-		$(this).find("p").css("opacity", 1);
-		$(this).find("p").addClass("clicked");
-		if (click_count == 0) {
-			click_count = 1;
-			first_guess = $(this).find("p").html();
-		}
-		else {
-			click_count = 0
-			second_guess = $(this).find("p").html();
-			if (first_guess != second_guess){
-				$(".clicked").each().css("opacity", 0);
-			};
-		};
-		});
-	$("#clear").on("click", function() {
-		$(this).css("color", "red");
-		$(document).each(".clicked").css("opacity", 0);
-	});
+  event.preventDefault();
+  var click_count = 0;
+  var first_guess = 0;
+  var second_guess = 0;
+  $(".card").on("click", function() {
+    $(this).find("p").css("opacity", 1);
+    $(this).find("p").addClass("clicked");
+    if (click_count == 0) {
+      click_count = 1;
+      first_guess = $(this).find("p").html();
+    }
+    else {
+      click_count = 0
+      second_guess = $(this).find("p").html();
+      if (first_guess != second_guess){
+        $(document).each(".clicked").css("opacity", 0);
+        };
+      };
+    });
+  $("#clear").on("click", function() {
+    $(this).css("color", "red");
+    $(document).each(".clicked").css("opacity", 0);
+  });
 });
