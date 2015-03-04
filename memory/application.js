1a5009163a1b2c354b1fcc4389e8d3236111d77a
jeiben <jeiben@kippnyc.org>
2015-03-04 15:54:09 -0500

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,22 +1,7 @@
 $(document).ready(function(){
   var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
-  function shuffle(array) {
-    var currentIndex = array.length, temporaryValue, randomIndex ;
-    // While there remain elements to shuffle...
-    while (0 !== currentIndex) {
-      // Pick a remaining element...
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex -= 1;
-      // And swap it with the current element.
-      temporaryValue = array[currentIndex];
-      array[currentIndex] = array[randomIndex];
-      array[randomIndex] = temporaryValue;
-    }
-    return array;
-  }
-  var shuffled_array = shuffle(card_array);
-  
-  for(i in shuffled_array){
-    $('#card_holder').append('<div class="card"><p>'+shuffled_array[i]+'</p></div>');
+
+  for(i in card_array){
+    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
   }
 });
