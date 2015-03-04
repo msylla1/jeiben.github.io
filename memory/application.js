08658e7d100f7cefe75ef9be51aa2cf7084aacc9
jeiben <jeiben@kippnyc.org>
2015-03-03 20:34:49 -0500

    Update application.js

--- memory/application.js
+++ memory/application.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
   for(i=1; i<20; i++){
-    $('#card_holder').append('<p class="card">'+i+'</p>');
+    $('#card_holder').append('<div class="card"><p>'+i+'</p></div>');
   }
 });
