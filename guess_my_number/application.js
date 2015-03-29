dd4f931a2d352e3991c6e60cb3cb95bfee46e97a
jeiben <jeiben@kippnyc.org>
2015-03-29 16:56:27 -0400

    Update application.js

--- guess_my_number/application.js
+++ guess_my_number/application.js
@@ -1,7 +1,24 @@
 $(document).ready(function(){
-  var card_array = ["fish", "fish", "turtle", "turtle", "pig", "pig", "dog", "dog", "cat", "cat", "snake", "snake", "bird", "bird", "goat", "goat", "hamster", "hamster", "dragon", "dragon"]
-
-  for(i in card_array){
-    $('#card_holder').append('<div class="card"><p>'+card_array[i]+'</p></div>');
-  }
+  //find a way for the program to choose a random number between 1 and 100, save this as a variable
+  
+  
+  //when the player clicks on the 'guess' button
+  $('button').on('click', function(){
+    //save their guess as a variable
+    
+    
+    //and compare this guess to the random number that the computer picked
+    //if the user guessed the correct number...
+    
+      //what happens if the guess is correct?
+      
+    //if the user guessed too high...
+    
+      //update the 'feedback' paragraph to tell them to guess lower
+      
+    //otherwise, the user guessed too low...
+    
+      //update the 'feedback' paragraph to tell them to guess higher
+    
+  });
 });
