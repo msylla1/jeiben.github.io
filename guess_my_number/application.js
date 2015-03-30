29af094cd80f0636600630e91625f1b2f4dff9a6
msylla1 <msylla@kippnyccp.org>
2015-03-30 13:20:45 -0400

    Update application.js

--- guess_my_number/application.js
+++ guess_my_number/application.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
   //find a way for the program to choose a random number between 1 and 100, save this as a variable
-  
+  Math.floor(Math.random()*100);
   
   //when the player clicks on the 'guess' button
   $('button').on('click', function(){
